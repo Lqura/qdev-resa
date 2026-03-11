f5bcb9f8497c34136db95bbfb2ef49eff6701bb2
Marek Felšöci <marek@felsoci.sk>
2026-03-11 15:01:59 +0100

    Prepare tests for implementation

--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -1,8 +1,9 @@
 import unittest
+from resa.resa import *
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+class ResaUnitTests(unittest.TestCase):
+  def test_bookMeetingRoom(self):
+    self.assertEqual(True, False)
 
 if __name__ == '__main__':
-    unittest.main()
+  unittest.main()
