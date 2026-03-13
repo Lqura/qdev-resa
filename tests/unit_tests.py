d7c3951c9512602c406455ac7ed15f45af7e590a
Laura Reis <22502380@me.uvsq.fr>
2026-03-13 15:22:24 +0100

    Added test for medium room

--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -4,6 +4,8 @@ from resa.resa import *
 class bookMeetingRoomUnitTests(unittest.TestCase):
   def test_small_room(self):
     self.assertEqual(bookMeetingRoom(5), Room.SMALL)
+  def test_medium_room(self):
+    self.assertEqual(bookMeetingRoom(27), Room.MEDIUM)
 
 if __name__ == '__main__':
   unittest.main()
