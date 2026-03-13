a3900ce25acfd263f7f2c7677ce0c588036d32bc
Laura Reis <22502380@me.uvsq.fr>
2026-03-13 15:25:25 +0100

    Added test for large room

--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -6,6 +6,8 @@ class bookMeetingRoomUnitTests(unittest.TestCase):
     self.assertEqual(bookMeetingRoom(5), Room.SMALL)
   def test_medium_room(self):
     self.assertEqual(bookMeetingRoom(27), Room.MEDIUM)
+  def test_large_room(self):
+    self.assertEqual(bookMeetingRoom(42)), Room.LARGE)
 
 if __name__ == '__main__':
   unittest.main()
